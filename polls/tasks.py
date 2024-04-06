039c3c5ec59d9db7159cb06db16b266a9cd8ec6e
Yunwen.Chen <yunwen.chen@acercsi.com>
2024-04-06 15:39:16 +0800

    Periodic Tasks

--- polls/tasks.py
+++ polls/tasks.py
@@ -34,3 +34,8 @@ def task_postrun_handler(task_id, **kwargs):
     the event and then send it to web client
     """
     notify_channel_layer(task_id)
+
+@shared_task(name='task_clear_session')
+def task_clear_session():
+    from django.core.management import call_command
+    call_command('clearsessions')
