41521358c035de43d3e55716cc2b927e09127d1a
Yunwen.Chen <yunwen.chen@acercsi.com>
2024-04-13 14:31:47 +0800

    Retrying Failed Tasks

--- polls/tasks.py
+++ polls/tasks.py
@@ -1,4 +1,6 @@
 import random
+
+import celery
 import requests
 from celery import shared_task
 from celery.signals import task_postrun
@@ -8,24 +10,38 @@ from polls.consumers import notify_channel_layer
 
 logger = get_task_logger(__name__)
 
+
+class BaseTaskWithRetry(celery.Task):
+    autoretry_for = (Exception, KeyError)
+    retry_kwargs = {'max_retries': 5}
+    retry_backoff = True
+    retry_jitter = True
+
 @shared_task()
 def sample_task(email):
     from polls.views import api_call
 
     api_call(email)
 
-@shared_task(bind=True)
+# @shared_task(bind=True)
+# def task_process_notification(self):
+#     try:
+#         if not random.choice([0, 1]):
+#             # mimic random error
+#             raise Exception()
+#
+#         # this would block the I/O
+#         requests.post('https://httpbin.org/delay/5')
+#     except Exception as e:
+#         logger.error('exception raised, it would be retry after 5 seconds')
+#         raise self.retry(exc=e, countdown=5)
+@shared_task(bind=True, base=BaseTaskWithRetry)
 def task_process_notification(self):
-    try:
-        if not random.choice([0, 1]):
-            # mimic random error
-            raise Exception()
+    if not random.choice([0, 1]):
+        # mimic random error
+        raise Exception()
 
-        # this would block the I/O
-        requests.post('https://httpbin.org/delay/5')
-    except Exception as e:
-        logger.error('exception raised, it would be retry after 5 seconds')
-        raise self.retry(exc=e, countdown=5)
+    requests.post('https://httpbin.org/delay/5')
 
 @task_postrun.connect
 def task_postrun_handler(task_id, **kwargs):
@@ -44,11 +60,9 @@ def task_clear_session():
 def dynamic_example_one():
     logger.info('Example One')
 
-
 @shared_task(name='low_priority:dynamic_example_two')
 def dynamic_example_two():
     logger.info('Example Two')
-
 
 @shared_task(name='high_priority:dynamic_example_three')
 def dynamic_example_three():
